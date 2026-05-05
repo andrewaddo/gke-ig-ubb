ea66c735bf6e355ffdef4ba6dc09ffb9c321c1ce
Andrew Do <ducdo@google.com>
2026-05-05 22:21:20 +0800

    fix(test): make queue monitor script output to terminal instead of background file

--- monitor_queue_depth.sh
+++ monitor_queue_depth.sh
@@ -2,8 +2,8 @@
 L4_IP=$(kubectl get pods -n default -l app=triton-recml,gpu=l4 -o jsonpath='{.items[0].status.podIP}')
 G4_IP=$(kubectl get pods -n default -l app=triton-recml,gpu=g4 -o jsonpath='{.items[0].status.podIP}')
 
-echo "Time     | L4 Queue | G4 Queue" > /tmp/queue_depth_log.txt
-echo "------------------------------" >> /tmp/queue_depth_log.txt
+echo "Time     | L4 Queue | G4 Queue"
+echo "------------------------------"
 
 while true; do
   L4_Q=$(kubectl exec perf-client -- curl -s $L4_IP:8002/metrics | awk '/nv_inference_pending_request_count\{/ {print $2}')
@@ -13,7 +13,7 @@ while true; do
   if [ -z "$G4_Q" ]; then G4_Q="0"; fi
   
   TIME=$(date +%H:%M:%S)
-  printf "%s | %-8s | %-8s\n" "$TIME" "$L4_Q" "$G4_Q" >> /tmp/queue_depth_log.txt
+  printf "%s | %-8s | %-8s\n" "$TIME" "$L4_Q" "$G4_Q"
   
   sleep 2
 done
